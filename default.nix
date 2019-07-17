ab17ae89d8c5668d1871a84238b91a674b0f85b3
Keith Collister <hnefatl@gmail.com>
2019-07-17 17:10:34 +0100

    Initial commit

---
+++ default.nix
@@ -0,0 +1,10 @@
+let
+    pkgs = import <nixpkgs> { };
+in
+    pkgs.stdenv.mkDerivation {
+        name = "rustup-faker";
+
+        # Don't extract from a source, just copy the script over
+        unpackPhase = "true";
+        installPhase = "mkdir -p $out && cp ${./rustup} $out/rustup";
+    }
